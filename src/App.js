6e9a1b3923510018a9921829d0474534c3bcf9d6
lunov08 <lukitmate@gmail.com>
2021-10-31 06:19:37 -0300

    configure Scroll component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import CardList from './CardList';
 import { robots } from './robots.js';
 import SearchBox from './SearchBox';
 import "./App.css";
+import Scroll from './Scroll';
 
 class App extends React.Component {
     constructor() {
@@ -33,8 +34,10 @@ class App extends React.Component {
             return(
                 <div className="tc"> 
                     <h1 className="f1">RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange}/> 
-                    <CardList robots={filteredRobots}/>
+                    <SearchBox searchChange={this.onSearchChange}/>  
+                    <Scroll>
+                        <CardList robots={filteredRobots}/>    
+                    </Scroll>
                 </div>
                 
             )
