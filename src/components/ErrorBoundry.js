652a84863bd63789770e92508d0433b1bad83c03
lunov08 <lukitmate@gmail.com>
2021-10-31 07:16:34 -0300

    configure ErrorBoundry component

---
+++ src/components/ErrorBoundry.js
@@ -0,0 +1,24 @@
+import React, { Component} from 'react';
+
+class ErrorBoundry extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            hasError: false
+        }
+    }
+
+componentDidCath(error, info) {
+    this.setState({ hasError: true })
+}
+
+render() {
+    if (this.state.hasError) {
+        return <h1>Something is wrong! Sorry :/</h1>
+    }
+    return this.props.children
+}
+
+}
+
+export default ErrorBoundry;
